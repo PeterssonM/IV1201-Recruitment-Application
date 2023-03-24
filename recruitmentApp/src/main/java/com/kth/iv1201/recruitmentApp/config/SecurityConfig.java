5f62e1938c907c34d8198c62c6d83f986fbc6c0d
Danielmebrahtu <79120174+Danielmebrahtu@users.noreply.github.com>
2023-03-24 15:20:20 +0100

    Homepage, logo & Navbar fixed

--- recruitmentApp/src/main/java/com/kth/iv1201/recruitmentApp/config/SecurityConfig.java
+++ recruitmentApp/src/main/java/com/kth/iv1201/recruitmentApp/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/logo/**");
     }
     
 
